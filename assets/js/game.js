2ea6f67799480242d74b5b257027f8af7d25aae3
Deisel <74831846+Amelia-was@users.noreply.github.com>
2020-12-14 12:13:01 -0500

    added high score storage

--- assets/js/game.js
+++ assets/js/game.js
@@ -137,7 +137,7 @@ var startGame = function() {
     for (var i = 0; i < enemyInfo.length; i++) {
         if (playerInfo.health > 0) {
             // tell player what round they are in
-            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1) + "\nYou have: " + playerInfo.health + " health; " + playerInfo.attack + " attack; " + playerInfo.money + " coins");
 
             // pick new enemy to fight from enemyInfo array
             var pickedEnemyObj = enemyInfo[i];
@@ -204,9 +204,25 @@ var shop = function () {
 // end game function
 var endGame = function() {
     window.alert("The game has now ended. Let's see how you did!");
+    
     // if player is still alive, player wins!
     if (playerInfo.health > 0) {
         window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+        // check localStorage for high score, if it's not there, use 0
+        var highScore = localStorage.getItem("highscore");
+        if (highScore === null) {
+            highScore = 0;
+        }
+        // if player have more money than the high score, player has new high score!
+        if (playerInfo.money > highScore) {
+            localStorage.setItem("highscore", playerInfo.money);
+            localStorage.setItem("name", playerInfo.name);
+
+            alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+        }
+        else {
+            alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+        }
     }
     // if player has died
     else {
